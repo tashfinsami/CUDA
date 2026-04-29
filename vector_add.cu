5a896e77caed5ab5e7e5519eb050239175f579cd
tashfinsami <tashfinsami@gmail.com>
2026-04-30 01:12:43 +0600

    Upload vector_add.cu

--- vector_add.cu
+++ vector_add.cu
@@ -2,7 +2,7 @@
 #include<cuda_runtime.h>
 const int n = 4;
 
-__global__ void vec_add(int* a, int* b, int* c) {
+__global__ void vec_add_kernel(int* a, int* b, int* c) {
     int i = threadIdx.x;
     c[i] = a[i] + b[i];
 }
@@ -11,19 +11,34 @@ int main() {
     int h_a[n] = {1, 2, 3, 4}, h_b[n] = {10, 20, 30,  40};
     int *d_a, *d_b, *d_c;
 
-    cudaMalloc(&d_a, n * sizeof(int));
-    cudaMemcpy(d_a, h_a, n * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMalloc(&d_b, n * sizeof(int));
-    cudaMemcpy(d_b, h_b, n * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMalloc(&d_c, n * sizeof(int));
+    cudaError_t err;
+    err = cudaMalloc(&d_a, n * sizeof(int));
+    if (err != cudaSuccess) printf("cudaMalloc d_a failed\n");
+    err = cudaMemcpy(d_a, h_a, n * sizeof(int), cudaMemcpyHostToDevice);
+    if (err != cudaSuccess) printf("cudaMemcpy h_a -> d_a failed: %s\n", cudaGetErrorString(err));
+    err = cudaMalloc(&d_b, n * sizeof(int));
+    if (err != cudaSuccess) printf("cudaMalloc d_b failed\n");
+    err = cudaMemcpy(d_b, h_b, n * sizeof(int), cudaMemcpyHostToDevice);
+    if (err != cudaSuccess) printf("cudaMemcpy h_b -> d_b failed: %s\n", cudaGetErrorString(err));
+    err = cudaMalloc(&d_c, n * sizeof(int));
+    if (err != cudaSuccess) printf("cudaMalloc d_c failed\n");
 
     int blk = 1, threads = n;
-    vec_add<<<blk, threads>>>(d_a, d_b, d_c);
+    vec_add_kernel<<<blk, threads>>>(d_a, d_b, d_c);
+    err = cudaGetLastError();
+    if (err != cudaSuccess) printf("Kernel launch failed: %s\n", cudaGetErrorString(err));
     cudaDeviceSynchronize();
 
     int h_c[n];
-    cudaMemcpy(h_c, d_c, n * sizeof(int), cudaMemcpyDeviceToHost);
+    err = cudaMemcpy(h_c, d_c, n * sizeof(int), cudaMemcpyDeviceToHost);
+    if (err != cudaSuccess) printf("cudaMemcpy d_c -> h_c failed: %s\n", cudaGetErrorString(err));
+
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
 
     for(int i = 0; i < n; i++) printf("%d ", h_c[i]);
+    printf("\n");
+
     return 0; 
 }
